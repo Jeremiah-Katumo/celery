266763308ee0347602586987344a40251445b0c9
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-07-26 01:02:49 +0300

    command signals

--- src/proj/moduleB/signals.py
+++ src/proj/moduleB/signals.py
@@ -1,6 +1,10 @@
 
-from celery.signals import after_task_publish, celeryd_after_setup
+from celery.signals import after_task_publish, celeryd_after_setup, celeryd_init
+from celery import Celery
+from celery import signals
+from celery.bin.base import Option
 
+from ..celery import app
 
 ### Task Signals
 @after_task_publish.connect(sender='tasks.add')
@@ -14,4 +18,35 @@ def task_sent_handler(sender=None, headers=None, body=None, **kwargs):
 
 
 ### Worker Signals
-@celeryd_after_setup.connect()
+@celeryd_after_setup.connect
+def setupp_direct_queue(sender, instance, **kwargs):
+    queue_name = '{0}.dq'.format(sender)  # sender is the nodename of the worker
+    instance.app.amqp.queues.select_Add(queue_name)
+
+@celeryd_init.connect(sender='worker@mail.com')
+def configure_workers(sender=None, conf=None, **kwargs):
+    conf.task_default_rate_limit = '10/m'
+
+    # Example: configure the number of workers based on the number of CPU cores
+    num_workers = len(conf.CELERY_WORKER_CONCURRENCY_LIMIT)
+    conf.CELERY_WORKER_CONCURRENCY = num_workers
+
+
+@celeryd_init.connect
+def configure_workers(sender=None, conf=None, **kwargs):
+    if sender in ('worker1@mail.com', 'worker2@mail.com'):
+        conf.task_default_rate_limit = '5/m'
+    if sender == 'worker4@mail.com':
+        conf.worker_prefetch_multiplier = 0
+
+
+### Command Signals
+app.user_options['preload'].add(Option(
+    '--monitoring', action='store_true',
+    help='Enable our external monitoring utility, blahblah',
+))
+
+@signals.user_preload_options.connect
+def handle_preload_options(options, **kwargs):
+    if options['monitoring']:
+        enable_monitoring()
