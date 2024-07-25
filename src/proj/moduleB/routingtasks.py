2d8bad7ef67772773ef6fbcd2c1d78783c34e259
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-07-25 21:21:20 +0300

    automatic routing

--- src/proj/moduleB/routingtasks.py
+++ src/proj/moduleB/routingtasks.py
@@ -1,7 +1,21 @@
 
+from ..celery import app
+
+
+### Automatic Routing
+app.conf.task_routes = {'feed.tasks.*': {'queue': 'feeds'}}
 
 task_routes = ([
     ('feed.tasks.*', {'queue': 'feeds'}),
     ('web.tasks.*', {'queue': 'web'}),
     (re.compile(r'(video|image)\.tasks\..*'), {'queue': 'media'}),
 ],)
+
+app.conf.task_default_queue = 'default'
+
+# A queue named “video” will be created with the following settings:
+{'exchange': 'video',
+ 'exchange_type': 'direct',
+ 'routing_key': 'video'}
+
+### Manual Routing
